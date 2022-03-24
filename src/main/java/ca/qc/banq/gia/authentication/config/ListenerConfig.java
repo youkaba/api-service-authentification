1068710e4566911a434bb3e328a6b3872d31aac7
ykaba <youssoufa.kaba@banq.qc.ca>
2022-03-24 15:38:42 -0400

    some refactor

--- src/main/java/ca/qc/banq/gia/authentication/config/ListenerConfig.java
+++ src/main/java/ca/qc/banq/gia/authentication/config/ListenerConfig.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package ca.qc.banq.gia.authentication.config;
 
@@ -32,44 +32,41 @@ import static java.util.function.Predicate.not;
 @Profile("local")
 public class ListenerConfig extends ContextLoaderListener {
 
-	private final AppRepository appRepo;
-	
-	/**
-	 * Initialisation d'une liste dapplications par defaut pour des tests
-	 */
-	App[] initApps = new App[] {
-			new App("68fb0e8c-7754-49ed-a7a7-7c084b9b7bd9", "idel-extranet-web-app", TypeAuth.B2C, "http://localhost:8080/idel-local/extranet/redirectVersAccueilExtranet.seam", "gPpG_DVWH4KV9B42eoNZVOg7~p4-u8iQ-M", "B2C_1_signinsignup_idel_extranet", "", "", "",false),
-			new App("e1ddda0b-7ce2-42e0-82c4-c849986f69e0", "idel-intranet-web-app", TypeAuth.AAD, "http://localhost:8080/idel-local/intranet/IntranetAccueil.seam", "aRa~rBhcr_b1q6q3_W26g5fbyk~1sB629.", "", "", "", "",false)
-	};
-	
-	/* (non-Javadoc)
-	 * @see javax.servlet.ServletContextListener#contextInitialized(javax.servlet.ServletContextEvent)
-	 */
-	@Override
-	public void contextInitialized(ServletContextEvent arg0) {
-		initApp();
-	}
+    private final AppRepository appRepo;
 
-	/* (non-Javadoc)
-	 * @see javax.servlet.ServletContextListener#contextDestroyed(javax.servlet.ServletContextEvent)
-	 */
-	@Override
-	public void contextDestroyed(ServletContextEvent arg0) {}
+    /**
+     * Initialisation d'une liste dapplications par defaut pour des tests
+     */
+    App[] initApps = new App[]{
+            new App("68fb0e8c-7754-49ed-a7a7-7c084b9b7bd9", "idel-extranet-web-app", TypeAuth.B2C, "http://localhost:8080/idel-local/extranet/redirectVersAccueilExtranet.seam", "gPpG_DVWH4KV9B42eoNZVOg7~p4-u8iQ-M", "B2C_1_signinsignup_idel_extranet", "", "", "", false),
+            new App("e1ddda0b-7ce2-42e0-82c4-c849986f69e0", "idel-intranet-web-app", TypeAuth.AAD, "http://localhost:8080/idel-local/intranet/IntranetAccueil.seam", "aRa~rBhcr_b1q6q3_W26g5fbyk~1sB629.", "", "", "", "", false)
+    };
 
+    /* (non-Javadoc)
+     * @see javax.servlet.ServletContextListener#contextInitialized(javax.servlet.ServletContextEvent)
+     */
+    @Override
+    public void contextInitialized(ServletContextEvent arg0) {
+        initApp();
+    }
 
-	/**
-	 * Initialisation des donnees
-	 */
-	@Transactional
-	void initApp() {
-		try {
+    @Override
+    public void contextDestroyed(ServletContextEvent event) {
+        super.contextDestroyed(event);
+    }
 
-			Arrays.stream(initApps)
-					.filter(not(app -> appRepo.existsById(app.getClientId())))
-					.forEach(appRepo::save);
-		} catch (Exception e) {
-			log.error(" initialization has failed because of " + e.getMessage());
-		}
-	}
-	
+    /**
+     * Initialisation des donnees
+     */
+    @Transactional
+    void initApp() {
+        try {
+            Arrays.stream(initApps)
+                    .filter(not(app -> appRepo.existsById(app.getClientId())))
+                    .forEach(appRepo::save);
+        } catch (Exception e) {
+            log.error(" initialization has failed because of " + e.getMessage());
+        }
+    }
+
 }
